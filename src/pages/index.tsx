11f8ff123ef3f6a12eda427fa4deff0a91d5734a
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-08-01 13:26:35 +0700

    chore: add swr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,41 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Link as ChakraLink, Text } from "@chakra-ui/react";
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Hero } from "../components/Hero";
+import { Container } from "../components/Container";
+import { Main } from "../components/Main";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { CTA } from "../components/CTA";
+import { Footer } from "../components/Footer";
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text color="text">
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
+import useSWR from "swr";
 
-      <List spacing={3} my={0} color="text">
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+const Index = () => {
+  const { data, error } = useSWR(
+    "https://api.github.com/repos/vercel/swr",
+    fetcher
+  );
 
-export default Index
+  if (error) return "An error has occured.";
+  if (!data) return "Loading...";
+
+  return (
+    <Container height="100vh">
+      <Hero />
+      <Main>
+        <Text color="text">Subscribers {data.subscribers_count}</Text>
+        <Text color="text">Forks {data.forks_count}</Text>
+        <Text color="text">Stargazers {data.stargazers_count}</Text>
+      </Main>
+
+      <DarkModeSwitch />
+      <Footer>
+        <Text>Next ❤️ Chakra</Text>
+      </Footer>
+      <CTA />
+    </Container>
+  );
+};
+
+export default Index;
