214231cee9245cf522b55bce0705bb815b7704f9
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-08-04 14:36:00 +0700

    feat: create main layout component

---
+++ src/components/layouts/Main.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import { Box, Container } from "@chakra-ui/react";
+import Navbar from "../Navbar";
+
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Home - Anime Book</title>
+      </Head>
+
+      <Navbar path={router.asPath} />
+
+      <Container maxW="container.md" pt={14}>
+        {children}
+      </Container>
+    </Box>
+  );
+};
+
+export default Main;
