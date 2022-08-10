8f31d950a7d061a2b1bced5a0fa97c9733e7dd81
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-08-10 15:24:00 +0700

    feat: create conditional data rendering method for upcoming section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
+import { useState } from "react";
+import { Button, Container, Heading, SimpleGrid } from "@chakra-ui/react";
 import Layout from "../components/layouts/Main";
 import Section from "../components/Section";
 import { GridItem } from "../components/GridItem";
@@ -6,7 +7,12 @@ import { GridItem } from "../components/GridItem";
 import { getUpcoming } from "../lib/queriesList";
 
 export default function Index() {
+  const [showAll, setShowAll] = useState(false);
   const { upcomingDatas, upcomingError } = getUpcoming();
+
+  const changeShowAllValue = () => {
+    setShowAll(!showAll);
+  };
 
   if (upcomingError) return "An error has occured.";
 
@@ -16,18 +22,43 @@ export default function Index() {
         <Container maxW="container.xl">
           <Heading as="h3" fontSize={20} mt={8} mb={4}>
             Upcoming
+            <span>
+              <Button
+                ml={4}
+                size="sm"
+                background="#E4BAD4"
+                color="whiteAlpha.900"
+                onClick={changeShowAllValue}
+              >
+                Show All
+              </Button>
+            </span>
           </Heading>
 
           <SimpleGrid columns={[1, 3, 5]} gap={8}>
-            {upcomingDatas.data.map((data) => (
-              <Section key={data.mal_id}>
-                <GridItem
-                  id={data.title}
-                  title={data.title}
-                  thumbnail={data.images.webp.image_url}
-                />
-              </Section>
-            ))}
+            {showAll
+              ? upcomingDatas.data.map((data, index) => {
+                  return (
+                    <Section key={index}>
+                      <GridItem
+                        id={data.title}
+                        title={data.title}
+                        thumbnail={data.images.webp.image_url}
+                      />
+                    </Section>
+                  );
+                })
+              : upcomingDatas.data.slice(0, 5).map((data, index) => {
+                  return (
+                    <Section key={index}>
+                      <GridItem
+                        id={data.title}
+                        title={data.title}
+                        thumbnail={data.images.webp.image_url}
+                      />
+                    </Section>
+                  );
+                })}
           </SimpleGrid>
         </Container>
       </Layout>
