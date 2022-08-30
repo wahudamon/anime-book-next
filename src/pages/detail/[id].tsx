64d8d9860d0b23412ab388e3bf28c3c9c3c44714
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-08-30 14:06:00 +0700

    refactor: hide score section and set container height value

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -42,159 +42,161 @@ export default function AnimeDetail() {
     // console.log(animeDetailsData.data);
     return (
       <Layout title={animeDetailsData.data.title} router="/">
-        <Box position="fixed" top={0} left={0} w="100%" zIndex={5}>
-          {animeDetailsData.data.trailer.images.maximum_image_url ? (
-            <Image
-              src={animeDetailsData.data.trailer.images.maximum_image_url}
-              width="full"
-              height="xl"
-              filter="blur(4px)"
-            />
-          ) : (
-            <Box></Box>
-          )}
-
-          <Box
-            position="absolute"
-            top={0}
-            left={0}
-            w="100%"
-            h="100%"
-            background="#F6DFEB"
-            opacity={
-              animeDetailsData.data.trailer.images.maximum_image_url ? 0.6 : 1
-            }
-          >
+        <Container h="800px">
+          <Box position="fixed" top={0} left={0} w="100%" zIndex={5}>
             {animeDetailsData.data.trailer.images.maximum_image_url ? (
-              <div></div>
-            ) : (
-              <h2
-                style={{
-                  fontWeight: "bold",
-                  fontSize: "24px",
-                  textAlign: "center",
-                }}
-              >
-                No Image Available.
-              </h2>
-            )}
-          </Box>
-        </Box>
-        <Container
-          position="fixed"
-          left={0}
-          top={400}
-          pb={4}
-          maxW="full"
-          maxH="full"
-          zIndex={15}
-          background="#F6DFEB"
-        >
-          <Box display="grid" gridTemplateColumns="300px auto auto" gap={6}>
-            <Box mt={-20} maxW="fit-content">
               <Image
-                src={animeDetailsData.data.images.jpg.image_url}
-                w="300px"
-                h="400px"
-                fit="cover"
-                borderRadius="14px"
+                src={animeDetailsData.data.trailer.images.maximum_image_url}
+                width="full"
+                height="xl"
+                filter="blur(4px)"
               />
-            </Box>
-            <Box display="flex" flexDirection="column">
-              <Box mt={-10}>
-                <h2 style={{ fontWeight: "bold", fontSize: "24px" }}>
-                  {animeDetailsData.data.title
-                    ? animeDetailsData.data.title.length > 60
-                      ? `${animeDetailsData.data.title.slice(0, 60)}...`
-                      : animeDetailsData.data.title
-                    : "NA"}
-                </h2>
-              </Box>
-              <Box>
-                <Box
-                  mt="8px"
-                  display="flex"
-                  gap={2}
-                  alignItems="flex-end"
-                  justifyContent="space-between"
-                  maxW="container.xl"
-                >
-                  <h3
-                    style={{
-                      fontWeight: "bold",
-                      fontSize: "20px",
-                    }}
-                  >
-                    Synopsis
-                  </h3>
-                  <Box
-                    display="flex"
-                    gap={4}
-                    style={{ fontWeight: "bold", fontSize: "20px" }}
-                  >
-                    <h3>
-                      Status:{" "}
-                      {animeDetailsData.data.status
-                        ? animeDetailsData.data.status
-                        : "NA"}
-                    </h3>
-                    <h3>
-                      Rank:{" "}
-                      {animeDetailsData.data.rank
-                        ? `#${animeDetailsData.data.rank}`
-                        : "NA"}
-                    </h3>
-                    <h3>
-                      Popularity:{" "}
-                      {animeDetailsData.data.popularity
-                        ? `#${animeDetailsData.data.popularity}`
-                        : "NA"}
-                    </h3>
-                  </Box>
-                </Box>
-                <p
+            ) : (
+              <Box></Box>
+            )}
+
+            <Box
+              position="absolute"
+              top={0}
+              left={0}
+              w="100%"
+              h="100%"
+              background="#F6DFEB"
+              opacity={
+                animeDetailsData.data.trailer.images.maximum_image_url ? 0.6 : 1
+              }
+            >
+              {animeDetailsData.data.trailer.images.maximum_image_url ? (
+                <div></div>
+              ) : (
+                <h2
                   style={{
-                    marginTop: "12px",
-                    float: "left",
-                    fontSize: "14px",
-                    textAlign: "justify",
-                    fontWeight: "400",
+                    fontWeight: "bold",
+                    fontSize: "24px",
+                    textAlign: "center",
                   }}
                 >
-                  {animeDetailsData.data.synopsis
-                    ? animeDetailsData.data.synopsis
-                    : "No synopsis found."}
-                </p>
+                  No Image Available.
+                </h2>
+              )}
+            </Box>
+          </Box>
+          <Container
+            position="fixed"
+            left={0}
+            top={400}
+            pb={4}
+            maxW="full"
+            h="full"
+            zIndex={15}
+            background="#F6DFEB"
+          >
+            <Box display="grid" gridTemplateColumns="300px auto auto" gap={6}>
+              <Box mt={-20} maxW="fit-content">
+                <Image
+                  src={animeDetailsData.data.images.jpg.image_url}
+                  w="300px"
+                  h="400px"
+                  fit="cover"
+                  borderRadius="14px"
+                />
+              </Box>
+              <Box display="flex" flexDirection="column">
+                <Box mt={-10}>
+                  <h2 style={{ fontWeight: "bold", fontSize: "24px" }}>
+                    {animeDetailsData.data.title
+                      ? animeDetailsData.data.title.length > 60
+                        ? `${animeDetailsData.data.title.slice(0, 60)}...`
+                        : animeDetailsData.data.title
+                      : "NA"}
+                  </h2>
+                </Box>
+                <Box>
+                  <Box
+                    mt="8px"
+                    display="flex"
+                    gap={2}
+                    alignItems="flex-end"
+                    justifyContent="space-between"
+                    maxW="container.xl"
+                  >
+                    <h3
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: "20px",
+                      }}
+                    >
+                      Synopsis
+                    </h3>
+                    <Box
+                      display="flex"
+                      gap={4}
+                      style={{ fontWeight: "bold", fontSize: "20px" }}
+                    >
+                      <h3>
+                        Status:{" "}
+                        {animeDetailsData.data.status
+                          ? animeDetailsData.data.status
+                          : "NA"}
+                      </h3>
+                      <h3>
+                        Rank:{" "}
+                        {animeDetailsData.data.rank
+                          ? `#${animeDetailsData.data.rank}`
+                          : "NA"}
+                      </h3>
+                      <h3>
+                        Popularity:{" "}
+                        {animeDetailsData.data.popularity
+                          ? `#${animeDetailsData.data.popularity}`
+                          : "NA"}
+                      </h3>
+                    </Box>
+                  </Box>
+                  <p
+                    style={{
+                      marginTop: "12px",
+                      float: "left",
+                      fontSize: "14px",
+                      textAlign: "justify",
+                      fontWeight: "400",
+                    }}
+                  >
+                    {animeDetailsData.data.synopsis
+                      ? animeDetailsData.data.synopsis
+                      : "No synopsis found."}
+                  </p>
+                </Box>
               </Box>
             </Box>
-          </Box>
-          <Box
-            mt={4}
-            maxW="fit-content"
-            maxH="fit-content"
-            background="orange.200"
-            borderRadius="14px"
-          >
-            <Box
-              px={4}
-              py={2}
-              display="flex"
-              flexDirection="column"
-              textAlign="center"
+            {/* <Box
+              mt={4}
+              maxW="fit-content"
+              maxH="fit-content"
+              background="orange.200"
+              borderRadius="14px"
             >
-              <p style={{ fontWeight: "bold", fontSize: "18px" }}>SCORE</p>
-              <p style={{ fontWeight: "bold", fontSize: "32px" }}>
-                {animeDetailsData.data.score
-                  ? animeDetailsData.data.score
-                  : "NA"}
-              </p>
-              <p style={{ fontSize: "14px" }}>
-                {animeDetailsData.data.scored_by
-                  ? `${animeDetailsData.data.scored_by} Users`
-                  : "0 Users"}
-              </p>
-            </Box>
-          </Box>
+              <Box
+                px={4}
+                py={2}
+                display="flex"
+                flexDirection="column"
+                textAlign="center"
+              >
+                <p style={{ fontWeight: "bold", fontSize: "18px" }}>SCORE</p>
+                <p style={{ fontWeight: "bold", fontSize: "32px" }}>
+                  {animeDetailsData.data.score
+                    ? animeDetailsData.data.score
+                    : "NA"}
+                </p>
+                <p style={{ fontSize: "14px" }}>
+                  {animeDetailsData.data.scored_by
+                    ? `${animeDetailsData.data.scored_by} Users`
+                    : "0 Users"}
+                </p>
+              </Box>
+            </Box> */}
+          </Container>
         </Container>
       </Layout>
     );
